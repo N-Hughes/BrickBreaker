b21eac6f7b79fc09392930eebb5e139b0e8396d3
Cam Grosse <camegros192@ed.amdsb.ca>
2023-04-21 09:41:15 -0400

    created a method

--- BrickBreaker/Paddle.cs
+++ BrickBreaker/Paddle.cs
@@ -7,7 +7,7 @@ namespace BrickBreaker
         public int x, y, width, height, speed;
         public Color colour;
 
-        public Paddle(int _x, int _y, int _width, int _height, int _speed, Color _colour)
+        public Paddle( int _x, int _y, int _width, int _height, int _speed, Color _colour)
         {
             x = _x;
             y = _y;
